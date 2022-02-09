e9482270b663e4da3dd4fb51d4344c47215332c4
Yew Chong <83965035+yewey2@users.noreply.github.com>
2022-02-09 02:12:06 -0500

    implicit wait for html to load

--- main.py
+++ main.py
@@ -143,13 +143,14 @@ def set_user_data(chat_id=None, username="", password="", dynamodb=None):
 def main(username='', password='',):
     options = webdriver.ChromeOptions() 
     options.add_argument("start-maximized")
-    #options.add_argument('--headless')
+    options.add_argument('--headless')
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument("--disable-blink-features=AutomationControlled")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)#, executable_path=r'C:\bin\chromedriver.exe') #edit path to webdriver
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.implicitly_wait(5)
 
 
     driver.get('https://eacct-purdue-sp.transactcampus.com/purdueeaccounts/AnonymousHome.aspx')
