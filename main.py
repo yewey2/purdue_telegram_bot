a909d86175ff7fcb5a66238dc25fa85cdb03cd94
Yew Chong <83965035+yewey2@users.noreply.github.com>
2022-02-11 13:28:53 -0500

    Import random debug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
