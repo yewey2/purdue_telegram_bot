a0625180b1e4d0c1557756487296c5f92503ab08
Yew Chong <83965035+yewey2@users.noreply.github.com>
2022-02-19 21:42:12 -0500

    db controller location changed

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 # import time
 import os
-import boto3
-from botocore.exceptions import ClientError
-# import db_controller as db
+import db_controller.db_controller as db
 
 from telegram.ext import (
     Updater,
@@ -40,105 +38,6 @@ from cryptography.fernet import Fernet
 FERNET_KEY = os.environ.get('FERNET_KEY') # This is a string
 key_b = FERNET_KEY.encode('utf-8') # Convert to Bytes
 CIPHER_SUITE = Fernet(FERNET_KEY)
-
-def create_table(dynamodb=None):
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb',
-            aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_KEY,
-            region_name=REGION_NAME,
-        )
-    try:
-        table = dynamodb.create_table(
-            TableName=TABLE_NAME,
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'chat_id',
-                    'AttributeType': 'N'
-                },
-                # {
-                #     'AttributeName': 'username',
-                #     'AttributeType': 'S'
-                # },
-                # {
-                #     'AttributeName': 'password',
-                #     'AttributeType': 'S'
-                # },
-            ],
-            KeySchema=[
-                {
-                    'AttributeName': 'chat_id',
-                    'KeyType': 'HASH'  
-                },
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            }
-        )
-        return table
-    except ClientError:
-        return None
-    finally:
-        return None
-
-def get_user_data(chat_id=None, dynamodb=None):
-    """Getting single entry"""
-    if not chat_id:
-        return None
-    create_table()
-    if not dynamodb:
-        dynamodb = boto3.resource(
-            'dynamodb',
-            aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_KEY,
-            region_name=REGION_NAME,
-        )
-    table = dynamodb.Table(TABLE_NAME)
-    response = None
-    try:
-        response = table.get_item(Key={'chat_id': chat_id})['Item']
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    except KeyError as e:
-        print('Key Error! Item not found')
-        print(e)
-    finally:
-        return response
-
-def set_user_data(chat_id=None, username="", password="", dynamodb=None):
-    """Updating single entry"""
-    if not chat_id:
-        return None
-    create_table()
-    if not dynamodb:
-        dynamodb = boto3.resource(
-            'dynamodb',
-            aws_access_key_id=ACCESS_KEY,
-            aws_secret_access_key=SECRET_KEY,
-            region_name=REGION_NAME,
-        )
-    table = dynamodb.Table(TABLE_NAME)
-    try:
-        response = table.update_item(
-            Key={
-                'chat_id': chat_id,
-            },
-            UpdateExpression="set username=:u, password=:p",
-            ExpressionAttributeValues={
-                ':u': username,
-                ':p': password
-            },
-            ReturnValues="UPDATED_NEW"
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-    except KeyError as e:
-        print('Key Error! Item not found')
-        print(e)
-    finally:
-        return response
-
 
 def main(username='', password='',):
     options = webdriver.ChromeOptions() 
@@ -299,7 +198,7 @@ dispatcher.add_handler(ohno_handler)
 
 def swipes_command(update,context):
     """Get meal swipes"""
-    user_data = get_user_data(update.message.from_user.id)
+    user_data = db.get_user_data(update.message.from_user.id)
     if not user_data:
         text = 'Sorry, please /login first!'
         context.bot.send_message(
@@ -365,7 +264,7 @@ def login_done(update,context):
     username = context.user_data['username']
     password = CIPHER_SUITE.encrypt(bytes(password, 'utf-8')) # Encrypt password before storing
     chat_id = update.message.from_user.id
-    set_user_data(chat_id, username, password)
+    db.set_user_data(chat_id, username, password)
     context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"\
